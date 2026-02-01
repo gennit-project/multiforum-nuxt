26aaa979ff25ba6be7683e923797108c0072050d
Catherine Luse <gennitdev@gmail.com>
2026-02-01 15:53:42 -0700

    Wrap accept invite page in client only

--- pages/forums/[forumId]/accept-invite.vue
+++ pages/forums/[forumId]/accept-invite.vue
@@ -3,15 +3,24 @@ import PendingInvite from '@/components/mod/PendingInvite.vue';
 import RequireAuth from '@/components/auth/RequireAuth.vue';
 </script>
 <template>
-  <RequireAuth>
-    <template #has-auth>
-      <PendingInvite />
-    </template>
-    <template #does-not-have-auth>
-      <div class="flex flex-col items-center justify-center dark:text-white">
-        <h1 class="text-2xl">Accept Invite</h1>
-        <p class="text-lg">You must be logged in to accept an invite.</p>
+  <ClientOnly>
+    <RequireAuth>
+      <template #has-auth>
+        <PendingInvite />
+      </template>
+      <template #does-not-have-auth>
+        <div class="flex flex-col items-center justify-center dark:text-white">
+          <h1 class="text-2xl">Accept Invite</h1>
+          <p class="text-lg">You must be logged in to accept an invite.</p>
+        </div>
+      </template>
+    </RequireAuth>
+    <template #fallback>
+      <div class="flex flex-col items-center justify-center py-8">
+        <div
+          class="h-8 w-8 animate-spin rounded-full border-4 border-blue-500 border-t-transparent"
+        />
       </div>
     </template>
-  </RequireAuth>
+  </ClientOnly>
 </template>
