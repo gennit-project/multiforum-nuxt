b32eccd1bd5726001e76ab0c311834bf50298a19
Catherine Luse <gennitdev@gmail.com>
2026-06-21 16:36:40 -0700

    Fix forum hydration query variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -381,7 +381,6 @@ export default defineNuxtConfig({
     },
     define: {
       global: 'globalThis',
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true', // TEMP DIAGNOSTIC
     },
     vue: {
       template: {
